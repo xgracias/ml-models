35aa16e95e11a5f56462c4dd106bf68d48f2dcdd
xgracias <49041460+xgracias@users.noreply.github.com>
2025-03-12 05:32:40 -0700

    add multiple linear regression

---
+++ multiple_linear_regression/multiple_linear_regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# encode categorical data
+ct = ColumnTransformer(transformers=['encoder', OneHotEncoder(), [3]], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# train model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
