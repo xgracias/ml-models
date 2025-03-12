dc1cd7d76af1c013227cfad07fc5d2dbd0e5a542
xgracias <49041460+xgracias@users.noreply.github.com>
2025-03-12 05:53:00 -0700

    The transformers parameter of ColumnTransformer expects a list of tuples, not a list of individual elements

--- multiple_linear_regression/multiple_linear_regression.py
+++ multiple_linear_regression/multiple_linear_regression.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # encode categorical data
-ct = ColumnTransformer(transformers=['encoder', OneHotEncoder(), [3]], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
 # split data
@@ -20,3 +20,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 # train model
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# predict the result
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
