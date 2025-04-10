b3c82f78ec244e5fcb005ebeb1b2acb65ff63f4a
xgracias <49041460+xgracias@users.noreply.github.com>
2025-04-09 20:09:46 -0700

    get number of clusters using elbow method

--- clustering/k-means/k-means.py
+++ clustering/k-means/k-means.py
@@ -5,4 +5,18 @@ import pandas as pd
 # import data set
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3, 4]].values
-y = dataset.iloc[:, -1].values
+
+# find the optimal number of clusters using elbow method
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init = 'k-means++', random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+## visualize wcss
+plt.plot(range(1,11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
