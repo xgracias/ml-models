bc216bca5949a5a361da2a94b8622959175a13af
xgracias <49041460+xgracias@users.noreply.github.com>
2025-04-10 17:14:05 -0700

    finish hierarchical clustering

--- clustering/hierarchical/hierarchical.py
+++ clustering/hierarchical/hierarchical.py
@@ -9,9 +9,30 @@ X = dataset.iloc[:, [3, 4]].values
 print(X)
 
 # use dendrogram to find the optimal number of clusters
+'''
 import scipy.cluster.hierarchy as sch
 dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
 plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean distances')
+plt.show()
+'''
+
+# train
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_hc = hc.fit_predict(X) # train and return clusters
+
+print(y_hc)
+
+# visualize
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s=100, c='magenta', label='Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
 plt.show()
