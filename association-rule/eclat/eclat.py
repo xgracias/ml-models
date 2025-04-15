a13dab02ff4c8fffad5c35013289f1cd6b7a0241
xgracias <49041460+xgracias@users.noreply.github.com>
2025-04-15 08:02:24 -0700

    add eclat

---
+++ association-rule/eclat/eclat.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+# train
+from apyori import apriori
+## 0.003 = 3 times * 7 weeks / 7501
+rules = apriori(transactions = transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length = 2, max_length=2)
+
+# visualize
+results = list(rules)
+
+## putting results into pandas data frame
+def inspect(results):
+    lhs         = [tuple(result[2][0][0])[0] for result in results]
+    rhs         = [tuple(result[2][0][1])[0] for result in results]
+    supports    = [result[1] for result in results]
+    return list(zip(lhs, rhs, supports))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
+# print(resultsinDataFrame)
+
+# Displaying the results sorted by descending supports
+resultsinDataFrame = resultsinDataFrame.nlargest(n = 10, columns = 'Support')
+print(resultsinDataFrame)
