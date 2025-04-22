a4ff149e91c46f7efd19c1a08e990fb101bb9639
xgracias <49041460+xgracias@users.noreply.github.com>
2025-04-22 07:37:31 -0700

    finish bag of words + naive bayes

--- natural-language-processing/bag-of-words/bag-of-words.py
+++ natural-language-processing/bag-of-words/bag-of-words.py
@@ -32,3 +32,28 @@ for i in range(0, 1000):
     corpus.append(review)
 
 print(corpus)
+
+# create bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, -1].values
+
+print(len(X[0]))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# train naive bayes model
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+acc_score = accuracy_score(y_test, y_pred)
+print(acc_score)
