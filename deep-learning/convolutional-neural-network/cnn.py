bde98d5c2d4907c2f8499ba72c2ee8faf1735b15
xgracias <49041460+xgracias@users.noreply.github.com>
2025-04-30 02:57:52 -0700

    image preprocessing with keras

--- deep-learning/convolutional-neural-network/cnn.py
+++ deep-learning/convolutional-neural-network/cnn.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-tf.__version__
+print(tf.__version__)
+
+# processing the train set
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64, 64), batch_size=32, class_mode='binary')
+
+# processing the test set
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64, 64), batch_size=32, class_mode='binary')
